f89b7bdb76fe043175efdeef57285cbb96fc7ef5
HmodyXD <135520176+HmoodyXD@users.noreply.github.com>
2024-03-29 08:35:01 +0100

    totally bad code (iam too lazy)

--- src/main/java/com/hysteria/practice/game/tournament/commands/subcommands/TournamentJoinCommand.java
+++ src/main/java/com/hysteria/practice/game/tournament/commands/subcommands/TournamentJoinCommand.java
@@ -1,23 +1,5 @@
 package com.hysteria.practice.game.tournament.commands.subcommands;
 
-import com.hysteria.practice.game.tournament.Tournament;
-import com.hysteria.practice.game.tournament.impl.TournamentClans;
-import com.hysteria.practice.game.tournament.impl.TournamentSolo;
-import com.hysteria.practice.game.tournament.impl.TournamentTeams;
-import com.hysteria.practice.player.profile.Profile;
-import com.hysteria.practice.api.command.BaseCommand;
-import com.hysteria.practice.api.command.Command;
-import com.hysteria.practice.api.command.CommandArgs;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-
-/**
- * @author Hysteria Development
- * @project Practice
- * @date 2/12/2023
- */
-package com.hysteria.practice.game.tournament.commands.subcommands;
-
 import com.hysteria.practice.player.profile.Profile;
 import com.hysteria.practice.api.command.BaseCommand;
 import com.hysteria.practice.api.command.Command;
@@ -80,7 +62,7 @@ public class TournamentJoinCommand extends BaseCommand {
         }
     }
 
-    private void joinTournament(Player player, Tournament<?> tournament) {
+    private <T extends Tournament<?>> void joinTournament(Player player, T tournament) {
         if (tournament == null) {
             player.sendMessage(ChatColor.RED + "This type of tournament is not available.");
             return;
