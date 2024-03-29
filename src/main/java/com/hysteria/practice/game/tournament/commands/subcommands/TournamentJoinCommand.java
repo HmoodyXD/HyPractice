37fc5ebd811ca32a7f8cefb53d81b0697b01beed
HmodyXD <135520176+HmoodyXD@users.noreply.github.com>
2024-03-29 08:33:56 +0100

    upload bad code

--- src/main/java/com/hysteria/practice/game/tournament/commands/subcommands/TournamentJoinCommand.java
+++ src/main/java/com/hysteria/practice/game/tournament/commands/subcommands/TournamentJoinCommand.java
@@ -16,16 +16,73 @@ import org.bukkit.entity.Player;
  * @project Practice
  * @date 2/12/2023
  */
+package com.hysteria.practice.game.tournament.commands.subcommands;
+
+import com.hysteria.practice.player.profile.Profile;
+import com.hysteria.practice.api.command.BaseCommand;
+import com.hysteria.practice.api.command.Command;
+import com.hysteria.practice.api.command.CommandArgs;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import com.hysteria.practice.game.tournament.impl.TournamentSolo;
+import com.hysteria.practice.game.tournament.impl.TournamentTeams;
+import com.hysteria.practice.game.tournament.impl.TournamentClans;
+import com.hysteria.practice.game.tournament.Tournament;
+
+/**
+ * @author Hysteria Development
+ * @project Practice
+ * @date 2/12/2023
+ */
+
 public class TournamentJoinCommand extends BaseCommand {
 
     @Command(name = "tournament.join")
     @Override
     public void onCommand(CommandArgs commandArgs) {
         Player player = commandArgs.getPlayer();
-        Tournament<?> tournament = Tournament.getTournament();
+        String[] args = commandArgs.getArgs();
 
+        if (args.length > 0) {
+            String tournamentType = args[0].toLowerCase();
+            switch (tournamentType) {
+                case "solo":
+                    joinTournament(player, new TournamentSolo());
+                    break;
+                case "teams":
+                    joinTournament(player, new TournamentTeams());
+                    break;
+                case "clans":
+                    joinTournament(player, new TournamentClans());
+                    break;
+                default:
+                    player.sendMessage(ChatColor.RED + "Invalid tournament type. Use: solo, teams, or clans.");
+                    break;
+            }
+        } else {
+            Tournament<?> activeTournament = Tournament.getTournament();
+            if (activeTournament == null) {
+                player.sendMessage(ChatColor.RED + "There is no active tournament at the moment.");
+                return;
+            }
+
+            Profile profile = Profile.get(player.getUniqueId());
+            if (profile.isBusy()) {
+                player.sendMessage(ChatColor.RED + "You may not join the tournament in your current state.");
+                return;
+            }
+            if (profile.isInTournament()) {
+                player.sendMessage(ChatColor.RED + "You are already in a tournament.");
+                return;
+            }
+
+            activeTournament.join(player);
+        }
+    }
+
+    private void joinTournament(Player player, Tournament<?> tournament) {
         if (tournament == null) {
-            player.sendMessage(ChatColor.RED + "No tournament found.");
+            player.sendMessage(ChatColor.RED + "This type of tournament is not available.");
             return;
         }
 
@@ -35,21 +92,10 @@ public class TournamentJoinCommand extends BaseCommand {
             return;
         }
         if (profile.isInTournament()) {
-            player.sendMessage(ChatColor.RED + "You are already in the tournament.");
+            player.sendMessage(ChatColor.RED + "You are already in a tournament.");
             return;
         }
 
-        if (tournament instanceof TournamentSolo) {
-            TournamentSolo soloTournament = (TournamentSolo) tournament;
-            soloTournament.join(player);
-        } else if (tournament instanceof TournamentClans) {
-            TournamentClans clansTournament = (TournamentClans) tournament;
-            clansTournament.join(player);
-        } else if (tournament instanceof TournamentTeams) {
-            TournamentTeams teamsTournament = (TournamentTeams) tournament;
-            teamsTournament.join(player);
-        } else {
-            player.sendMessage(ChatColor.RED + "This command is not applicable for the current tournament type.");
-        }
+        tournament.join(player);
     }
 }
